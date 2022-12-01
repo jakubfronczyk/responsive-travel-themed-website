611d60df385e065429a8c4f95439efa58ce3b76e
Jakub Fronczyk <jakubfronczyk@gmail.com>
2022-12-01 13:24:11 +0100

    Added Hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,50 +1,29 @@
-import React, { Fragment } from "react";
-import Button from "./../../UI/Button/Button";
+import React from 'react'
+import Button from '../../UI/Button/Button'
+import coverVideo from './../../assets/cover.mp4'
+import classes from './Hero.module.scss'
 
-import coverVID from "./../../assets/cover.mp4";
-
-import classes from "./Hero.module.scss";
-
-const HomePageContent = () => {
-    return (
-        <Fragment>
-            <h1 className={classes.hero__content__title}>the tropics are waiting</h1>
-            <span className={classes.hero__content__tagline}>
-                Let us know when you're finished packing
-            </span>
-            <p className={classes.hero__content__description}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere beatae itaque ea
-                ducimus, mollitia voluptate laudantium.
-            </p>
-            <div className={classes.hero__content__cta}>
-                <Button to="/booknow">Book Now</Button>
-                <Button to="/learnmore" outline>
-                    Learn More
-                </Button>
-            </div>
-        </Fragment>
-    );
-};
-
-const Hero = ({ isDynmic, children }) => {
+const Hero = () => {
     return (
         <div className={classes.container}>
-            <video
-                autoPlay={false}
-                muted
-                loop
-                className={classes.video}
-                id={"video"}
-                src={coverVID}
-            ></video>
+            <video muted loop className={classes.video} id={"video"} src={coverVideo}></video>
             <div className={classes.hero}>
                 <div className={classes.hero__content}>
-                    {!isDynmic && <HomePageContent />}
-                    {isDynmic && <h1 className={classes.hero__content__title}>{children}</h1>}
+                    <h1 className={classes.hero__content__title}>The tropics are wainting</h1>
+                    <span className={classes.hero__content__tagline}>
+                        Let us know when you're finished packing
+                    </span>
+                    <p className={classes.hero__content__description}>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptu
+                    </p>
+                    <div className={classes.hero__content__cta}>
+                        <Button>Book Now</Button>
+                        <Button outline>Learn More</Button>
+                    </div>
                 </div>
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default Hero;
+export default Hero
