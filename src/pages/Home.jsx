7f5a646f4f68caa87e07aa64c4f8058a7e5bc742
Jakub Fronczyk <jakubfronczyk@gmail.com>
2022-12-02 22:04:15 +0100

    Added Locations Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React, { Fragment } from 'react'
+import Hero from '../components/Hero/Hero'
+import Locations from '../components/Locations/Locations'
 
 const Home  = () => {
     return (
         <Fragment>
             <Hero />
+            <Locations />
         </Fragment>
     )
 }
